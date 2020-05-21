d55c15601a9a8ee4b78dd374e8c1bdca524d4517
Vinay Kumar G.B <vinaykumargb3@gmail.com>
2020-05-21 14:19:54 +0530

    Updates

---
+++ precache-manifest.dd284d1c472babe4dc9d604ea1c0768e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602f417ea77a223c09b227130099de97",
+    "url": "/COVID-19-tracker/index.html"
+  },
+  {
+    "revision": "88e2b0b31590685c9571",
+    "url": "/COVID-19-tracker/static/css/main.69296995.chunk.css"
+  },
+  {
+    "revision": "dc89339e18ef5ee73841",
+    "url": "/COVID-19-tracker/static/js/2.92931b4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/COVID-19-tracker/static/js/2.92931b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e2b0b31590685c9571",
+    "url": "/COVID-19-tracker/static/js/main.1d984f0f.chunk.js"
+  },
+  {
+    "revision": "104390ffa4a17d20f231",
+    "url": "/COVID-19-tracker/static/js/runtime-main.9be7c9d7.js"
+  }
+]);
